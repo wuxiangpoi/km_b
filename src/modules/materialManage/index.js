46eb20fab73d633c3094390be640a4258ab04566
wuxiang <303152175@qq.com>
2018-06-11 13:45:59 +0800

    0611

--- src/modules/materialManage/index.js
+++ src/modules/materialManage/index.js
@@ -243,65 +243,6 @@ const materialManageController = ($rootScope, $scope, $filter, baseService, moda
                 fileItem.file.desc = fileName.join(',');
             };
 
-            vm.uploader.onBeforeUploadItem = function (item) {
-                
-                var imgfile_type = $rootScope.getRootDicNameStrs('image_format');
-                var videofile_type = $rootScope.getRootDicNameStrs('video_format');
-                var host = '';
-                var accessid = '';
-                var policyBase64 = '';
-                var signature = '';
-                var callbackbody = '';
-                var filename = '';
-                var key = '';
-                //	var	 expire = 0;
-                var token = '';
-                var ctype = item.file.name.substr(item.file.name.lastIndexOf('.') + 1).toLowerCase();
-                var type = ',' + ctype + ',';
-                var xType = '';
-                if ((',' + imgfile_type.toLowerCase() + ',').indexOf(type) != -1) {
-                    xType = 0;
-                } else if ((',' + videofile_type.toLowerCase() + ',').indexOf(type) != -1) {
-                    xType = 1;
-                } else {
-                    xType = 2;
-                }
-                baseService.postData(baseService.api.material + 'addMaterial_getOssSignature', {
-                    type: xType
-                }, function (obj) {
-                    host = obj['host']
-                    policyBase64 = obj['policy']
-                    accessid = obj['accessid']
-                    signature = obj['signature']
-                    //	expire =obj['expire']
-                    callbackbody = obj['callback']
-                    key = obj['key']
-                    token = obj['token']
-                    //	$scope.uploader.url=host;
-                    var filename = item.file.name;
-                    if (item.file['desc']) {
-                        filename = item.file.desc;
-                    }
-
-                    var new_multipart_params = {
-                        'key': (key + item.file.name.substr(item.file.name.lastIndexOf('.'))),
-                        'policy': policyBase64,
-                        'OSSAccessKeyId': accessid,
-                        'success_action_status': '200', //让服务端返回200,不然，默认会返回204
-                        'callback': callbackbody,
-                        'signature': signature,
-                        'x:fname': filename,
-                        'x:type': xType,
-                        'x:gid': item.oid,
-                        'x:opt': 0,
-                        'x:token': token
-                    };
-                    item.formData = [new_multipart_params]; //上传前，添加描述文本
-                    item.upload();
-                });
-
-
-            }
             vm.uploader.onCompleteItem = function (fileItem, response, status, headers) {
                 if (response) {
                     if (response.code != 1) {
