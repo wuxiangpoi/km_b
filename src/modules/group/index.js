ef6e04157ec1b4f5939003b5173fdc386df27941
wuxiang <303152175@qq.com>
2018-06-20 10:32:34 +0800

    0620

--- src/modules/group/index.js
+++ src/modules/group/index.js
@@ -45,22 +45,23 @@ const groupController = ($scope, $rootScope, baseService,modalService) => {
 	}
 	$scope.sumbitSort = function () {
 		var zTree = $.fn.zTree.getZTreeObj('groupSet');
-		baseService.postData(baseService.api.organization + 'saveOrganizationSort', {
-			sorts: JSON.stringify(zTree.getNodes()[0].children)
-		}, function (res) {
-			if (res) {
-				$scope.root_organizations = $rootScope.userData.root_organizations = res;
-				$scope.isSort = false;
-				$scope.ztreeSetting = {
-					zNodes: getOrganizations(),
-					isSort: false,
-					isSet: true,
-					isCheck: false,
-					selectedNodes: []
-				}
-			}
-
-		})
+		if(zTree.getNodes()[0].children){
+            baseService.postData(baseService.api.organization + 'saveOrganizationSort', {
+                sorts: JSON.stringify(zTree.getNodes()[0].children)
+            }, function (res) {
+                $scope.root_organizations = $rootScope.userData.root_organizations = res;
+                $scope.isSort = false;
+                $scope.ztreeSetting = {
+                    zNodes: getOrganizations(),
+                    isSort: false,
+                    isSet: true,
+                    isCheck: false,
+                    selectedNodes: []
+                }
+            })
+        }else{
+            $scope.isSort = false;
+        }
 	}
 	$scope.$on('addNode', function (e, zTree, treeNode) {
 		modalService.confirmDialog(540, '新建组织机构', {}, '/static/tpl/group_save.html', function (vm) {
